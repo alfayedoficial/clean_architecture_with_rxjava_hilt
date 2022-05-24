283492edc15b82ea580b9ce040d6d687509fd5a0
Ali Al Fayed <32135935+alfayedoficial@users.noreply.github.com>
2022-05-24 16:37:48 +0200

    Update Constants.kt

--- app/src/main/java/com/fourdev/cleanarchitecturewithrxjavahilt/utils/Constants.kt
+++ app/src/main/java/com/fourdev/cleanarchitecturewithrxjavahilt/utils/Constants.kt
@@ -7,11 +7,11 @@ package com.fourdev.cleanarchitecturewithrxjavahilt.utils
  */
 object Constants {
 
-    const val clientId = "BDSRKPX4QNCAJYUF4HFZBIUC5SYKZSG1NLY0UPSXDBJISOW3"
-    const val clientSecret = "5S5VAHATQWMGLBL12QEF0LOTFYXY5N3GZ4D02ZXA2MRY4JKT"
 
-    const val BASE_URL = "https://fsq-v3-demo.herokuapp.com/"
-//    const val BASE_URL = "http://aligntech2022-001-site1.ctempurl.com/api/v1/hr/"
+
+
+
+
     const val API_VERSION = "20220523"
     const val query_value = "RESTURANT"
     const val sort_value = "RELEVANCE"
